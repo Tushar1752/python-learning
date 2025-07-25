460bf9404490e4002cf26820eab1b33ac3fdc86d
Tushar1752 <tusharchaudhari8919@gmail.com>
2025-07-25 19:35:24 +0530

    Add files via upload

---
+++  calculator1.py
@@ -0,0 +1,14 @@
+num1=int(input("enter the first numbeerz:"))
+num2=int(input("enter the second numberz:"))
+add=num1+num2
+print ("the sum of the numbers is:",add)
+sub=num1-num2
+print("the difference of the number is:",sub)
+mult=num1*num2
+print("the product of the number is:",mult)
+div=num1/num2
+print("the division of the numbwer is :",div)
+mod=num1%num2
+print("the modulus of the number is:",mod)
+exp=num1**num2
+print("the exponent of the number is :",exp)
